00d3ffec6c65a7951615f3f239c6ebc41d91445a
Lewis Nixon <lewcfn@gmail.com>
2012-06-25 17:14:08 +0200

    changing the entry meta terminology

--- wordpress/wp-content/themes/bd_2012/functions.php
+++ wordpress/wp-content/themes/bd_2012/functions.php
@@ -16,3 +16,23 @@ function bd_common_scripts() {
 add_action('wp_enqueue_scripts', 'bd_common_scripts');
 
 register_nav_menu( 'main', 'Main Menu' );
+
+if ( ! function_exists( 'twentyeleven_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ * Create your own twentyeleven_posted_on to override in a child theme
+ *
+ * @since Twenty Eleven 1.0
+ */
+function twentyeleven_posted_on() {
+	printf( __( '<span class="sep">Published </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'twentyeleven' ),
+		esc_url( get_permalink() ),
+		esc_attr( get_the_time() ),
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+		esc_attr( sprintf( __( 'View all posts by %s', 'twentyeleven' ), get_the_author() ) ),
+		get_the_author()
+	);
+}
+endif;
