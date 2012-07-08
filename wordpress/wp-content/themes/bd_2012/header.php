04f5ba2d50779861d19ceb3f8ec7c6d997abfa2c
Lewis Nixon <lewcfn@gmail.com>
2012-07-08 19:55:40 +0100

    adding typekit support

--- wordpress/wp-content/themes/bd_2012/header.php
+++ wordpress/wp-content/themes/bd_2012/header.php
@@ -28,7 +28,8 @@
 
   <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
 	<link rel="stylesheet/less" href="<?php bloginfo('stylesheet_directory')?>/style.css">
-
+    <script type="text/javascript" src="http://use.typekit.com/elc7rhr.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   <!-- All JavaScript at the bottom, except this Modernizr build. -->
 
   <?php wp_head() ?>
