1e9cbce040704ee7413437b21540f2157e2fc7f7
Lewis Nixon <lewcfn@gmail.com>
2012-07-08 21:30:13 +0100

    more styles and a minor change to the copy

--- wordpress/wp-content/themes/bd_2012/content.php
+++ wordpress/wp-content/themes/bd_2012/content.php
@@ -27,7 +27,7 @@
 			</div><!-- .entry-summary -->
 			<?php else : ?>
 			<div class="entry-content">
-				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
+				<?php the_excerpt( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
 				<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 			</div><!-- .entry-content -->
 			<?php endif; ?> 
