9b7901fb5bc720b085865e0e0526fdf73f2812cf
Lewis Nixon <lewcfn@gmail.com>
2012-07-17 18:11:39 +0100

    more work on the archives sidebar and also instatiating the JS namespacing pattern

--- wordpress/wp-content/themes/bd_2012/js/script.js
+++ wordpress/wp-content/themes/bd_2012/js/script.js
@@ -1,7 +1,18 @@
-/* Author:
+/* Author: Lewis Nixon
 
 */
 
+var BD = BD || {};  
+// here a namespace object is passed as a function  
+// parameter, where we assign public methods and  
+// properties to it  
+(function( o ){  
+    o.foo = "foo";  
+    o.bar = function(){  
+        return "bar";  
+    };  
+})(BD);  
+console.log(BD);
 
 
 
