07ba41db74e6e3b972d9cb1b5ec26bce9147458d
Lewis Nixon <lewcfn@gmail.com>
2012-07-30 18:29:46 +0100

    adding the JS for switching the sidebar and re-positioning the sidebar

--- wordpress/wp-content/themes/bd_2012/js/script.js
+++ wordpress/wp-content/themes/bd_2012/js/script.js
@@ -6,23 +6,52 @@ var BD_2012 = BD_2012 || {};
 // here a namespace object is passed as a function  
 // parameter, where we assign public methods and  
 // properties to it  
-(function( o ){  
+(function( o ){
+  // Let's start with some variables
+  var $mainAside = $('#main-aside'),
+    $dataToggle = $('.data-toggle','#page'),
+    resizeTimer, w;
+    
+    // Now for the functions
     o.showArchives = function() {
-      $('.data-toggle','#page').click(function() {
+      $dataToggle.click(function() {
         var $t = $(this);
-        if ( $t.text() === "Archives" ) {
+        if ( $t.text() === 'Archives' ) {
           $t.text('Hide Archives');
         }
-        else if ( $t.text() === "Hide Archives" ) {
+        else if ( $t.text() === 'Hide Archives' ) {
           $t.text('Archives');
         }
-        $('#main-aside').toggleClass('shown');
+        $mainAside.toggleClass('shown');
+      });
+    };
+    
+    o.layeredArchiveFlag = function() {
+      if ( window.innerWidth ) {
+        w = window.innerWidth;
+        if ( w <= 1235 ) {
+          $mainAside.insertAfter($dataToggle);
+        }
+        else if ( w > 1235 ) {
+          $mainAside.insertBefore('#colophon');
+        }
+      }
+    };
+    
+    o.windowResizeListener = function() {
+      // Listen for the window.resize event
+      // but throttle it to fire only every
+      // 250ms to increase performance
+      $(window).on("resize load", function() {
+          clearTimeout(resizeTimer);
+          resizeTimer = setTimeout(o.layeredArchiveFlag, 250);
       });
     };    
 })(BD_2012);
 
 jQuery(function($) {
   BD_2012.showArchives();
+  BD_2012.windowResizeListener();
 });
 
 
