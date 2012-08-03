6fa5c3379e8be91614ff5b311840f22b309dfcab
Lewis Nixon <lewcfn@gmail.com>
2012-08-03 10:32:42 +0100

    merge conflict in footer

--- wordpress/wp-content/themes/bd_2012/header.php
+++ wordpress/wp-content/themes/bd_2012/header.php
@@ -38,7 +38,7 @@
   <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
        chromium.org/developers/how-tos/chrome-frame-getting-started -->
   <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-<div id="wrapper">
+<div id="wrapper" class="outer-wrap">
   <div id="page-outer" class="hfeed">
     <?php if ( is_single() ) { ?>
       <nav id="nav-single">
