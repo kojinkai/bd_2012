9981cbc841c2f01043676e0ad5694cb5d96b323b
Lewis Nixon <lewcfn@gmail.com>
2012-08-12 15:13:47 +0100

    tidying up the markup and adding the profile image

--- wordpress/wp-content/themes/bd_2012/about-page.php
+++ wordpress/wp-content/themes/bd_2012/about-page.php
@@ -7,19 +7,21 @@
 <?php get_header(); ?>
 
 	<div id="content" role="main">
-	<div class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>    	
-	</div>
-	<div id="profile-image">
-			
+		<div class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>    	
 		</div>
-		<div id="bio">
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php the_content(); ?>
-
-			<?php endwhile; // end of the loop. ?>
+		<div id="profile-image">
+			<div>
+			<img src="http://m4.licdn.com/mpr/mpr/shrink_100_100/p/4/000/131/2a7/3200eaa.jpg"  alt="lewis nixon profile image" />
+			</div>
 		</div>
+			<div id="bio">
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php the_content(); ?>
+
+				<?php endwhile; // end of the loop. ?>
+			</div>
 	</div> 
 
 <?php get_footer(); ?>
