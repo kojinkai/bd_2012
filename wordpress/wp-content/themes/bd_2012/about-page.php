3080ee6c6fce52920ee50340f23d134f187033a6
Lewis Nixon <lewcfn@gmail.com>
2012-10-16 18:09:26 +0100

    about page re-ordering

--- wordpress/wp-content/themes/bd_2012/about-page.php
+++ wordpress/wp-content/themes/bd_2012/about-page.php
@@ -6,22 +6,22 @@
 
 <?php get_header(); ?>
 
-	<div id="content" role="main">
+	<div id="content" class="clearfix" role="main">
 		<div class="entry-header">
 			<h1 class="entry-title"><?php the_title(); ?></h1>    	
 		</div>
-		<div id="profile-image">
-			<div>
-			<img src="http://m4.licdn.com/mpr/mpr/shrink_100_100/p/4/000/131/2a7/3200eaa.jpg"  alt="lewis nixon profile image" />
+		<div class="entry-content">
+			<div id="profile-image">
+				<img src="http://m4.licdn.com/mpr/mpr/shrink_100_100/p/4/000/131/2a7/3200eaa.jpg"  alt="lewis nixon profile image" />
 			</div>
-		</div>
 			<div id="bio">
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php the_content(); ?>
 
 				<?php endwhile; // end of the loop. ?>
-			</div>
+			</div>			
+		</div>
 	</div> 
 
 <?php get_footer(); ?>
