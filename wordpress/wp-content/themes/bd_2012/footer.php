ceed9a9f884eab91ce5cc92f4128bff6f7757764
Lewis Nixon <lewcfn@gmail.com>
2013-04-16 16:56:54 +0100

    removing the footer mugshot

--- wordpress/wp-content/themes/bd_2012/footer.php
+++ wordpress/wp-content/themes/bd_2012/footer.php
@@ -17,7 +17,6 @@
             }
           ?>
         </ul>
-        <a class="ir footer-mail-link" href="mailto:lewis@box-deluxe.com">lewis@box-deluxe.com</a>
       </div>
   	</footer><!-- #main-footer -->
   </div><!-- #page outer -->
