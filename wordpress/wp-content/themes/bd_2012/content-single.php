aea7195d4a60e4295b051839330c7ca88de27ccb
Lewis Nixon <lewcfn@gmail.com>
2013-04-26 14:11:33 +0100

    updating the new wrapper to be thinner - 760px

--- wordpress/wp-content/themes/bd_2012/content-single.php
+++ wordpress/wp-content/themes/bd_2012/content-single.php
@@ -18,6 +18,7 @@
 
 	<div class="entry-main clearfix">
 		<div class="entry-content">
+			<p class="summary-thought"><?php echo get_post_meta($post->ID, 'Summarising Thought', true); ?></p>
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
@@ -25,28 +26,6 @@
 		<footer class="entry-meta">
 			<?php
 				twentyeleven_posted_on();
-				/* translators: used between list items, there is a space after the comma */
-				// $categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
-
-				// /* translators: used between list items, there is a space after the comma */
-				// $tag_list = get_the_tag_list( '', __( ', ', 'twentyeleven' ) );
-				// if ( '' != $tag_list ) {
-				// 	$utility_text = __( 'in the categoy %1$s.', 'twentyeleven' );
-				// } elseif ( '' != $categories_list ) {
-				// 	$utility_text = __( 'in the categoy %1$s.', 'twentyeleven' );
-				// } else {
-				// 	$utility_text = __( 'This entry was posted by <a href="%6$s">%5$s</a>.', 'twentyeleven' );
-				// }
-
-				// printf(
-				// 	$utility_text,
-				// 	$categories_list,
-				// 	$tag_list,
-				// 	esc_url( get_permalink() ),
-				// 	the_title_attribute( 'echo=0' ),
-				// 	get_the_author(),
-				// 	esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
-				// );
 			?>
 			<?php // edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 
