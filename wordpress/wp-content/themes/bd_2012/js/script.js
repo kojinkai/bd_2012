994ba267195cc754c082fe4b64fe1dd61b154f22
Lewis Nixon <lewcfn@gmail.com>
2013-05-10 17:59:31 +0100

    adding the scrollTop shits to the contact shits

--- wordpress/wp-content/themes/bd_2012/js/script.js
+++ wordpress/wp-content/themes/bd_2012/js/script.js
@@ -7,51 +7,18 @@ var BD_2012 = BD_2012 || {};
 // parameter, where we assign public methods and  
 // properties to it  
 (function( o ){
-  // Let's start with some variables
-  var $mainAside = $('#main-aside'),
-    $dataToggle = $('.data-toggle','#page'),
-    resizeTimer, w;
-    
-    // Now for the functions
-    o.showArchives = function() {
-      $dataToggle.click(function() {
-        var $t = $(this);
-        if ( $t.text() === 'Archives' ) {
-          $t.text('Hide Archives');
-        }
-        else if ( $t.text() === 'Hide Archives' ) {
-          $t.text('Archives');
-        }
-        $mainAside.toggleClass('shown');
-      });
-    };
-    
-    o.layeredArchiveFlag = function() {
-      if ( window.innerWidth ) {
-        w = window.innerWidth;
-        if ( w <= 1235 ) {
-          $mainAside.insertAfter($dataToggle);
-        }
-        else if ( w > 1235 ) {
-          $mainAside.insertBefore('#site-info');
-        }
-      }
-    };
-    
-    o.windowResizeListener = function() {
-      // Listen for the window.resize event
-      // but throttle it to fire only every
-      // 250ms to increase performance
-      $(window).on("resize load", function() {
-          clearTimeout(resizeTimer);
-          resizeTimer = setTimeout(o.layeredArchiveFlag, 250);
-      });
-    };    
+
+  o.scrollToMugshot = function() {
+    $('li', '#menu-main-navigation').last().children('a').click(function() {
+      $('html, body').animate({
+        scrollTop: $("#lewis-nixon-headshot").offset().top
+      }, 1500);  
+    });    
+  }   
 })(BD_2012);
 
 jQuery(function($) {
-  BD_2012.showArchives();
-  BD_2012.windowResizeListener();
+  BD_2012.scrollToMugshot();
 });
 
 
